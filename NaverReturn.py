476f88ec3952ba798367c71884d42280d3e34e46
KELLY\yeseul <yesel6089@gmail.com>
2017-01-12 13:21:07 +0900

    Initial commit

---
+++ NaverReturn.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+
+import urllib
+from urllib import request
+
+# title, link, description 을 뱉는 모듈 만들기
+
+
+def get_title():
+    client_id = "YOUR_CLIENT_ID"
+    client_secret = "YOUR_CLIENT_SECRET"
+
+    # request parameter
+    display = '10'
+    start = '1000'
+
+    encText = urllib.parse.quote("안철수 대선")
+
+    # url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
+    url = "https://openapi.naver.com/v1/search/news.xml?query=" + encText + "&display=" + display + "&start=" + start  # xml 결과
+
+    print("url= " + url)
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", "oKYDp0sAGUP2ZaDMDX8z")
+    request.add_header("X-Naver-Client-Secret", "KHe_G13uer")
+
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+
+    if (rescode == 200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+        soup = BeautifulSoup(response_body.decode('utf-8'), "html.parser")
+
+        titles = soup.findAll('title')
+
+        title_list = []
+
+        for title in titles:
+            title = title.get_text()
+            title = title.replace('&', '')
+            title = title.replace('apos', '')
+            title = title.replace('quot;', '')
+            title = title.replace('<b>','')
+            title = title.replace('</b>','')
+
+            print(title)
+            title_list.append(title)
+            print("list of titles")
+            print(title_list)
+
+def get_link():
+    client_id = "YOUR_CLIENT_ID"
+    client_secret = "YOUR_CLIENT_SECRET"
+
+    # request parameter
+    display = '10'
+    start = '1000'
+
+    encText = urllib.parse.quote("안철수 대선")
+
+    # url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
+    url = "https://openapi.naver.com/v1/search/news.xml?query=" + encText + "&display=" + display + "&start=" + start  # xml 결과
+
+    print("url= " + url)
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", "oKYDp0sAGUP2ZaDMDX8z")
+    request.add_header("X-Naver-Client-Secret", "KHe_G13uer")
+
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+
+    if (rescode == 200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+        soup = BeautifulSoup(response_body.decode('utf-8'), "html.parser")
+
+        links = soup.findAll('link')
+
+        link_list = []
+
+        for link in links:
+            link = link.get_text()
+            print("link_list")
+            link_list.append(link)
+            print(link_list)
+
+def get_description():
+    client_id = "YOUR_CLIENT_ID"
+    client_secret = "YOUR_CLIENT_SECRET"
+
+    # request parameter
+    display = '10'
+    start = '1000'
+
+    encText = urllib.parse.quote("안철수 대선")
+
+    # url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
+    url = "https://openapi.naver.com/v1/search/news.xml?query=" + encText + "&display=" + display + "&start=" + start  # xml 결과
+
+    print("url= " + url)
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", "oKYDp0sAGUP2ZaDMDX8z")
+    request.add_header("X-Naver-Client-Secret", "KHe_G13uer")
+
+    response = urllib.request.urlopen(request)
+    rescode = response.getcode()
+
+    if (rescode == 200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+        soup = BeautifulSoup(response_body.decode('utf-8'), "html.parser")
+
+    descriptions = soup.findAll('description')
+
+    description_list = []
+
+    for description in descriptions:
+        description = description.get_text()
+        description = description.replace('&', '')
+        description = description.replace('apos', '')
+        description = description.replace('quot;', '')
+        description = description.replace('<b>', '')
+        description = description.replace('</b>', '')
+
+        print("description list ")
+
+        description_list.append(description)
+        print(description_list)
+
+def main():
+    get_title()
+    get_link()
+    get_description()
+
+if __name__ == '__main__':
+    main()
+
